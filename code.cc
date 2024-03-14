3776f0f6dfd86dccc750d567f36a7eafc00e8dfa
rondubi <rdubinsky@live.com>
2024-03-13 17:40:38 -0700

    IT COMPILES

--- code.cc
+++ code.cc
@@ -6,35 +6,44 @@
 
 namespace nfa
 {
+template <typename IteratorType>
 struct MiniNfa;
 };
 
+template <typename IteratorType>
 struct Predicate;
+
 struct Union;
 struct Concatenation;
 struct KleeneStar;
 
+template <typename IteratorType>
 struct GRVisitor
 {
-        std::shared_ptr<nfa::MiniNfa> visit(const Predicate *);
-        std::shared_ptr<nfa::MiniNfa> visit(const Union *);
-        std::shared_ptr<nfa::MiniNfa> visit(const Concatenation *);
-        std::shared_ptr<nfa::MiniNfa> visit(const KleeneStar *);
+        std::shared_ptr<nfa::MiniNfa<IteratorType>> visit(const Predicate<IteratorType> *);
+        std::shared_ptr<nfa::MiniNfa<IteratorType>> visit(const Union *);
+        std::shared_ptr<nfa::MiniNfa<IteratorType>> visit(const Concatenation *);
+        std::shared_ptr<nfa::MiniNfa<IteratorType>> visit(const KleeneStar *);
 };
 
 struct GeneralizedRegex
 {
-        virtual std::shared_ptr<nfa::MiniNfa> accept(GRVisitor * v) const = 0;
+        template <typename IteratorType>
+        std::shared_ptr<nfa::MiniNfa<IteratorType>> accept(GRVisitor<IteratorType> * v) const
+        {
+                return NULL;
+        }
 };
 
+template <typename IteratorType>
 struct Predicate : GeneralizedRegex
 {
         // TODO: will this actually work?
-        std::function<bool(std::any)> callable;
+        std::function<bool(IteratorType)> callable;
 
-        Predicate(std::function<bool(std::any)> c_) : callable(c_) { }
+        Predicate(std::function<bool(IteratorType)> c_) : callable(c_) { }
 
-        std::shared_ptr<nfa::MiniNfa> accept(GRVisitor * v) const
+        std::shared_ptr<nfa::MiniNfa<IteratorType>> accept(GRVisitor<IteratorType> * v) const
         {
                 return v->visit(this);
         }
@@ -49,7 +58,8 @@ struct Union : GeneralizedRegex
         {
         }
 
-        std::shared_ptr<nfa::MiniNfa> accept(GRVisitor * v) const
+        template <typename IteratorType>
+        std::shared_ptr<nfa::MiniNfa<IteratorType>> accept(GRVisitor<IteratorType> * v) const
         {
                 return v->visit(this);
         }
@@ -65,7 +75,8 @@ struct Concatenation : GeneralizedRegex
         {
         }
 
-        std::shared_ptr<nfa::MiniNfa> accept(GRVisitor * v) const
+        template <typename IteratorType>
+        std::shared_ptr<nfa::MiniNfa<IteratorType>> accept(GRVisitor<IteratorType> * v) const
         {
                 return v->visit(this);
         }
@@ -77,7 +88,8 @@ struct KleeneStar : GeneralizedRegex
 
         KleeneStar(GeneralizedRegex * o_) : operand(o_) { }
 
-        std::shared_ptr<nfa::MiniNfa> accept(GRVisitor * v) const
+        template <typename IteratorType>
+        std::shared_ptr<nfa::MiniNfa<IteratorType>> accept(GRVisitor<IteratorType> * v) const
         {
                 return v->visit(this);
         }
@@ -86,7 +98,8 @@ struct KleeneStar : GeneralizedRegex
 namespace impl
 {
 
-bool EPSILON(std::any unused_arg)
+template <typename IteratorType>
+bool EPSILON(IteratorType unused_arg)
 {
         return true;
 }
@@ -96,78 +109,86 @@ bool EPSILON(std::any unused_arg)
 namespace nfa
 {
 
+template <typename IteratorType>
 struct Transition;
 
+template <typename IteratorType>
 struct State
 {
         bool accept = false;
-        std::vector<Transition> out_edges{};
+        std::vector<Transition<IteratorType>> out_edges{};
 };
 
+template <typename IteratorType>
 struct Transition
 {
-        std::function<bool(std::any)> callable;
-        std::shared_ptr<State> to;
+        std::function<bool(IteratorType)> callable;
+        std::shared_ptr<State<IteratorType>> to;
 };
 
+template <typename IteratorType>
 struct MiniNfa
 {
-        State start_state{};
-        State end_state{};
+        State<IteratorType> start_state{};
+        State<IteratorType> end_state{};
 };
 
 }; // namespace nfa
 
 // NOTE: nicety: we know that predicates are going to be the leaf nodes of the syntax tree
-template <typename ArgType>
-std::shared_ptr<nfa::MiniNfa> GRVisitor::visit(const Predicate * p)
+template <typename IteratorType>
+std::shared_ptr<nfa::MiniNfa<IteratorType>> GRVisitor<IteratorType>::visit(const Predicate<IteratorType> * p)
 {
-        nfa::MiniNfa ret;
+        nfa::MiniNfa<IteratorType> ret;
 
         ret.start_state.out_edges.push_back({
                 .callable = p->callable,
-                .to = std::make_shared<nfa::State>(ret.end_state),
+                .to = std::make_shared<nfa::State<IteratorType>>(ret.end_state),
         });
 
-        return std::make_shared<nfa::MiniNfa>(ret);
+        return std::make_shared<nfa::MiniNfa<IteratorType>>(ret);
 }
 
-std::shared_ptr<nfa::MiniNfa> GRVisitor::visit(const Union * u)
+template <typename IteratorType>
+std::shared_ptr<nfa::MiniNfa<IteratorType>> GRVisitor<IteratorType>::visit(const Union * u)
 {
-        nfa::MiniNfa ret;
-        std::shared_ptr<nfa::State> end_ptr
-                = std::make_shared<nfa::State>(ret.end_state);
+        nfa::MiniNfa<IteratorType> ret;
+        std::shared_ptr<nfa::State<IteratorType>> end_ptr
+                = std::make_shared<nfa::State<IteratorType>>(ret.end_state);
 
-        std::shared_ptr<nfa::MiniNfa> lhs = u->lhs->accept(this);
+        std::shared_ptr<nfa::MiniNfa<IteratorType>> lhs = u->lhs->accept(this);
         lhs->end_state.out_edges.push_back({
-                .callable = impl::EPSILON,
+                .callable = impl::EPSILON<IteratorType>,
                 .to = end_ptr,
         });
-        std::shared_ptr<nfa::MiniNfa> rhs = u->rhs->accept(this);
+        std::shared_ptr<nfa::MiniNfa<IteratorType>> rhs = u->rhs->accept(this);
         rhs->end_state.out_edges.push_back({
-                .callable = impl::EPSILON,
+                .callable = impl::EPSILON<IteratorType>,
                 .to = end_ptr,
         });
 
-        return std::make_shared<nfa::MiniNfa>(ret);
+        return std::make_shared<nfa::MiniNfa<IteratorType>>(ret);
 }
 
-std::shared_ptr<nfa::MiniNfa> GRVisitor::visit(const Concatenation * c) { return NULL; }
+template <typename IteratorType>
+std::shared_ptr<nfa::MiniNfa<IteratorType>> GRVisitor<IteratorType>::visit(const Concatenation * c) { return NULL; }
 
-std::shared_ptr<nfa::MiniNfa> GRVisitor::visit(const KleeneStar * k) { return NULL; }
+template <typename IteratorType>
+std::shared_ptr<nfa::MiniNfa<IteratorType>> GRVisitor<IteratorType>::visit(const KleeneStar * k) { return NULL; }
 
 // NOTE: all this shit is utterly fucked and will not type check. Unfuck the NFA type hierarchy
 
-void traverse_and_print(std::shared_ptr<nfa::MiniNfa> expr, int indent = 0)
+template <typename IteratorType>
+void traverse_and_print(const std::shared_ptr<nfa::State<IteratorType>> state, int indent = 0)
 {
         for (int i = 0; i < indent; ++i)
                 std::cout << "\t";
 
-        std::cout << "Start state has " << expr.start_state.size()
+        std::cout << "Start state has " << state->out_edges.size()
                   << " out edges" << std::endl;
-        for (const auto [p, stage] : expr.start_state.out_edges)
+        for (const auto [p, stage] : state->out_edges)
         {
-                traverse_and_print(stage, indent + 1);
+                traverse_and_print<IteratorType>(stage, indent + 1);
                 std::cout << std::endl;
         }
         std::cout << "End subexpr" << std::endl;
@@ -175,13 +196,15 @@ void traverse_and_print(std::shared_ptr<nfa::MiniNfa> expr, int indent = 0)
 
 int main()
 {
-        GRVisitor v;
-        Predicate p1([](int x) { return x == 2; });
-        Predicate p2([](int x) { return x == 3; });
-        Union u(&p1, &p2);
-        std::shared_ptr<nfa::MiniNfa> res = u.accept(&v);
+        using It_T = std::vector<int>::const_iterator;
 
-        traverse_and_print(res);
+        GRVisitor<It_T> v;
+        Predicate<It_T> p1([](It_T x) { return *x == 2; });
+        Predicate<It_T> p2([](It_T x) { return *x == 3; });
+        Union u(&p1, &p2);
+        std::shared_ptr<nfa::MiniNfa<It_T>> res = u.accept(&v);
+
+        traverse_and_print<It_T>(std::make_shared<nfa::State<It_T>>(res->start_state));
         return 0;
 }
 
